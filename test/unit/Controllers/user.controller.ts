93441d590059ea57c73a20ca3caf5d62031c75cd
rcastro <cruzcastro07@gmail.com>
2020-03-26 22:40:27 +0000

    test: cover user.controller.ts with unit tests

---
+++ test/unit/Controllers/user.controller.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from '../../../src/Controllers/user.controller';
+import { buildUserWithId1 } from '../../helper/builder/user.build';
+
+describe('UserController', () => {
+  let app: TestingModule;
+  let userController: UserController;
+  let req: any = {};
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [UserController],
+    }).compile();
+
+    userController = app.get<UserController>(UserController);
+
+    req = {
+      user: {},
+      query: {},
+    };
+  });
+
+  describe('profileAction', () => {
+    const expectedResult = buildUserWithId1();
+    req.user = expectedResult;
+
+    expect(userController.profileAction(req)).toEqual(expectedResult);
+  });
+});
