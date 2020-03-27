2d4e3c1128bfaa31d32d1ed29c43f0ee678be053
rcastro <cruzcastro07@gmail.com>
2020-03-27 15:47:56 +0000

    test: cover translation.controller.ts with unit tests

---
+++ test/unit/Controllers/translation.controller.spec.ts
@@ -0,0 +1,172 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TranslationService } from '../../../src/Services/translation.service';
+import { TranslationController } from '../../../src/Controllers/translation.controller';
+import { TranslationDto } from '../../../src/Dto/translation.dto';
+import { InternalServerErrorException } from '@nestjs/common';
+
+describe('TranslationController', () => {
+  let app: TestingModule;
+  let translationController: TranslationController;
+  let translationService: TranslationService;
+  let req: any = {};
+  let res: any = {};
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [TranslationController],
+      providers: [
+        TranslationService,
+        {
+          provide: 'ApplicationService',
+          useValue: {},
+        },
+        {
+          provide: 'WhiteLabelService',
+          useValue: {},
+        },
+        {
+          provide: 'TranslationRepository',
+          useValue: {},
+        },
+        {
+          provide: 'LanguageService',
+          useValue: {},
+        },
+        {
+          provide: 'TranslationKeyService',
+          useValue: {},
+        },
+        {
+          provide: 'TranslationStatusService',
+          useValue: {},
+        },
+        {
+          provide: 'Connection',
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    translationController = app.get<TranslationController>(TranslationController);
+    translationService = app.get<TranslationService>(TranslationService);
+
+    req = {
+      user: {
+        companyId: '',
+      },
+      query: {},
+    };
+  });
+
+  beforeEach(() => {
+    res = {
+      header: {},
+      data: '',
+      setData(data) {
+        this.data = data;
+      },
+      getData() {
+        return this.data;
+      },
+      setHeader(name: string, value: string) {
+        this.header[name] = value;
+      },
+      getHeader(name) {
+        return this.header[name];
+      },
+      write(data: string, func: Function) {
+        this.setData(data);
+        func(undefined);
+      },
+      end() {
+        return;
+      },
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getTranslationsAction', () => {
+    it('should return a translation object', async () => {
+      const expectedResult = {
+        'translationKey': 'translation value',
+      };
+      const getTranslationsSpy = jest.spyOn(translationService, 'getTranslations').mockImplementation(async () => {
+        return expectedResult;
+      });
+
+      expect(await translationController.getTranslationsAction(req, new TranslationDto())).toEqual(expectedResult);
+      expect(getTranslationsSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getTranslationsJsonFile', () => {
+    it('should throw internal server error exception', async () => {
+      const expectedResult = {
+        'translationKey': 'translation value',
+      };
+
+      const getTranslationsSpy = jest.spyOn(translationService, 'getTranslations').mockImplementation(async () => {
+        return expectedResult;
+      });
+
+      res.write = (data: any, func: Function) => {
+        func({});
+      };
+
+      await expect(translationController.getTranslationsJsonFile(req, new TranslationDto(), res)).rejects.toThrow(InternalServerErrorException);
+      expect(getTranslationsSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return a json translation file', async () => {
+      const expectedResult = {
+        'translationKey': 'translation value',
+      };
+      const getTranslationsSpy = jest.spyOn(translationService, 'getTranslations').mockImplementation(async () => {
+        return expectedResult;
+      });
+
+      expect(await translationController.getTranslationsJsonFile(req, new TranslationDto(), res)).toBe(undefined);
+      expect(getTranslationsSpy).toHaveBeenCalledTimes(1);
+      expect(res.getHeader('Content-disposition')).toBe('attachment; filename=translation.json');
+      expect(res.getHeader('Content-type')).toBe('application/json');
+      expect(res.getData()).toBe(JSON.stringify(expectedResult));
+    });
+  });
+
+  describe('getTranslationsYamlFile', () => {
+    it('should throw internal server error exception', async () => {
+      const expectedResult = {
+        'translationKey': 'translation value',
+      };
+
+      const getTranslationsSpy = jest.spyOn(translationService, 'getTranslations').mockImplementation(async () => {
+        return expectedResult;
+      });
+
+      res.write = (data: any, func: Function) => {
+        func({});
+      };
+
+      await expect(translationController.getTranslationsYamlFile(req, new TranslationDto(), res)).rejects.toThrow(InternalServerErrorException);
+      expect(getTranslationsSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return a yml translation file', async () => {
+      const expectedResult = {
+        'translationKey': 'translation value',
+      };
+      const getTranslationsSpy = jest.spyOn(translationService, 'getTranslations').mockImplementation(async () => {
+        return expectedResult;
+      });
+
+      expect(await translationController.getTranslationsYamlFile(req, new TranslationDto(), res)).toBe(undefined);
+      expect(getTranslationsSpy).toHaveBeenCalledTimes(1);
+      expect(res.getHeader('Content-disposition')).toBe('attachment; filename=translation.yaml');
+      expect(res.getHeader('Content-type')).toBe('text/yaml');
+      expect(res.getData()).toBe(expectedResult);
+    });
+  });
+});
