c5539ea154b07ccd8d01c243c7d3f3d566491768
rcastro <cruzcastro07@gmail.com>
2020-04-26 20:46:12 +0100

    test: cover flat-index.node.spec.ts and nested-index.node.spec.ts with unit tests

---
+++ test/unit/Services/TranslationChainResponsability/Node/flat-index.node.spec.ts
@@ -0,0 +1,37 @@
+import { FlatIndexNode } from '../../../../../src/Services/TranslationChainResponsability/Node/flat-index.node';
+import { Translation } from '../../../../../src/Entities/translation.entity';
+import { buildTranslationWithId1 } from '../../../../helper/builder/translation.builder';
+import { buildTranslationKeyWithId1 } from '../../../../helper/builder/translation-key.build';
+
+describe('FlatIndexNode', () => {
+  let flatIndexNode: FlatIndexNode;
+
+  beforeAll(() => {
+    flatIndexNode = new FlatIndexNode();
+  });
+
+  describe('apply', () => {
+    it('should return translation request in with flat index', () => {
+      const translations: Translation[] = [];
+
+      const expectedResult = {
+        'qwe.rew.rte.ytry.rewr': 'lkzrsaghv esaprgj gnaog',
+        'qwe.rew.rte.ytry.wa4tcw': 'hy64by6 tpº,fog gnaog',
+        'qwe.rew.rte.eirgv': 'gjes hes0º jgam ºegvmnaºeirjg otenkfdjnbv la',
+        'ey.vfrthvr.ethyve.hter': 'lrsgjpoersjgpejhg ptesjh pºtesjhç  nbço gdçghu',
+      };
+
+      Object.keys(expectedResult).forEach(translationKeys => {
+        const translation = buildTranslationWithId1();
+        const translationKey = buildTranslationKeyWithId1();
+
+        translation.translation = expectedResult[translationKeys];
+        translationKey.alias = translationKeys;
+        translation.translationKey = translationKey;
+        translations.push(translation);
+      });
+
+      expect(flatIndexNode.apply(translations)).toEqual(expectedResult);
+    });
+  });
+});
---
+++ test/unit/Services/TranslationChainResponsability/Node/nested-index.node.spec.ts
@@ -0,0 +1,67 @@
+import { buildTranslationWithId1 } from '../../../../helper/builder/translation.builder';
+import { buildTranslationKeyWithId1 } from '../../../../helper/builder/translation-key.build';
+import { Translation } from '../../../../../src/Entities/translation.entity';
+import { NestedIndexNode } from '../../../../../src/Services/TranslationChainResponsability/Node/nested-index.node';
+
+describe('NestedIndexNode', () => {
+  let nestedIndex: NestedIndexNode;
+
+  beforeAll(() => {
+    nestedIndex = new NestedIndexNode();
+  });
+
+  describe('apply', () => {
+    it('should return translation request in with nested index', () => {
+
+      const translations: Translation[] = [];
+
+      const translationObject = {
+        'qwe.rew.rte.ytry.rewr': 'lkzrsaghv esaprgj gnaog',
+        'qwe.rew.rte.ytry.wa4tcw': 'hy64by6 tpº,fog gnaog',
+        'rwqcr.wqrwer.qwtrv.yt': 'fwarc gghr6u wd4 rq23r',
+        'ey.vfrthvr.ethyve.eirgv': 'gjes hes0º jgam ºegvmnaºeirjg otenkfdjnbv la',
+        'ey.vfrthvr.ethyve.hter': 'lrsgjpoersjgpejhg ptesjh pºtesjhç  nbço gdçghu',
+      };
+
+      Object.keys(translationObject).forEach(translationKeys => {
+        const translation = buildTranslationWithId1();
+        const translationKey = buildTranslationKeyWithId1();
+
+        translation.translation = translationObject[translationKeys];
+        translationKey.alias = translationKeys;
+        translation.translationKey = translationKey;
+        translations.push(translation);
+      });
+
+      expect(nestedIndex.apply(translations)).toEqual(
+        {
+          'qwe': {
+            'rew': {
+              'rte': {
+                'ytry': {
+                  'rewr': 'lkzrsaghv esaprgj gnaog',
+                  'wa4tcw': 'hy64by6 tpº,fog gnaog',
+                },
+              },
+            },
+          },
+          'rwqcr': {
+            'wqrwer': {
+              'qwtrv': {
+                'yt': 'fwarc gghr6u wd4 rq23r',
+              },
+            },
+          },
+          'ey': {
+            'vfrthvr': {
+              'ethyve': {
+                'eirgv': 'gjes hes0º jgam ºegvmnaºeirjg otenkfdjnbv la',
+                'hter': 'lrsgjpoersjgpejhg ptesjh pºtesjhç  nbço gdçghu',
+              },
+            },
+          },
+        },
+      );
+    });
+  });
+});
